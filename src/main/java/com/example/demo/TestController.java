e7c0ddb7c1fd2ea99fdf5a07acf692c40283e8c4
CareBiz\케어비즈 <hgm0302@nate.com>
2024-11-21 11:25:09 +0900

    1

--- src/main/java/com/example/demo/TestController.java
+++ src/main/java/com/example/demo/TestController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("/test")
     public String test(@RequestParam(value = "name", defaultValue = "World") String name) {
-        return String.format("Test1  %s!",name);
+        return String.format("Test2  %s!",name);
     }
 }
