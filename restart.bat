077be51eb2304cd16ce247cc699073cd2c237f54
CareBiz\케어비즈 <hgm0302@nate.com>
2024-11-21 13:18:17 +0900

    1

--- restart.bat
+++ restart.bat
@@ -26,7 +26,7 @@ IF NOT "%PID%"=="" (
 
 :: 새로운 JAR 실행 (백그라운드)
 echo Starting application: %JAR_PATH%
-start /B java -jar %JAR_PATH% > %LOG_PATH% 2>&1
+start /B java -jar %JAR_PATH%
 
 :: 대기 시간 (서버가 실행될 시간을 기다림)
 timeout /t 10 /nobreak >nul
